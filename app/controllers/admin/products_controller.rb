781344a2c65cc91aea54452e8d21b22983b1e12b
zhongxh <zhxhyz@gmail.com>
2017-05-10 09:56:15 +0800

    only admin can access backend panel

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,4 +1,9 @@
 class Admin::ProductsController < ApplicationController
+
+  layout "admin"
+  
+  before_action :authenticate_user!
+  before_action :admin_required
   def index
     @products = Product.all
   end
@@ -40,7 +45,7 @@ class Admin::ProductsController < ApplicationController
     redirect_to admin_products_path
     flash[:alert] = "商品已删除！"
   end
-  
+
     private
 
   def product_params
