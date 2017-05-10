0b6bc46df34f12f0e94b7b6a75eb52007810943a
zhongxh <zhxhyz@gmail.com>
2017-05-10 16:59:17 +0800

    add validation to products

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,5 @@
 class Product < ApplicationRecord
+  validates :title, :description, :quantity, :price, presence: true
+  validates :quantity, :price, numericality: { greater_than_or_equal_to: 0 }
   mount_uploader :image, ImageUploader
 end
