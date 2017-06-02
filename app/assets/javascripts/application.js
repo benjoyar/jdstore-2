6f2086b6c2f82e6e0985256c746fe87fc5418e97
zhongxh <zhxhyz@gmail.com>
2017-06-03 00:32:35 +0800

    add gotop

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,3 +15,30 @@
 //= require turbolinks
 //= require bootstrap
 //= require_tree .
+
+$(function() {
+    $("#usr_rmenu").each(function() {
+        $(this).find(".btn-top").click(function() {
+            $("html, body").animate({
+                "scroll-top": 0
+            }, 800)
+        })
+    });
+    var lastRmenuStatus = false;
+    $(window).scroll(function() {
+        var _top = $(window).scrollTop();
+        if (_top > 200) {
+            $("#usr_rmenu").data("expanded", true)
+        } else {
+            $("#usr_rmenu").data("expanded", false)
+        }
+        if ($("#usr_rmenu").data("expanded") != lastRmenuStatus) {
+            lastRmenuStatus = $("#usr_rmenu").data("expanded");
+            if (lastRmenuStatus) {
+                $("#usr_rmenu .btn-top").slideDown()
+            } else {
+                $("#usr_rmenu .btn-top").slideUp()
+            }
+        }
+    })
+});
