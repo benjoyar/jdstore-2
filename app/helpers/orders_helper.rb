b132c536bdfba7ca6ca75398ce7be50b462a1cb6
zhongxh <zhxhyz@gmail.com>
2017-06-05 16:30:54 +0800

    modify admin/orders/index

--- app/helpers/orders_helper.rb
+++ app/helpers/orders_helper.rb
@@ -7,7 +7,7 @@ module OrdersHelper
    end
  end
 
-  # 訂單狀態
+  # 账户订单状态
   def render_order_status(order)
     case order.aasm_state
     when 'order_placed'
@@ -24,4 +24,23 @@ module OrdersHelper
       t('订单已退货')
     end
   end
+
+  # 后台订单状态（管理者）
+  def render_admin_order_status(order)
+    case order.aasm_state
+    when 'order_placed'
+      t('用户已下单，等待付款')
+    when 'paid'
+      t('订单付款成功，准备出货')
+    when 'shipping'
+      t('订单出货中')
+    when 'shipped'
+      t('商品已送达')
+    when 'order_cancelled'
+      t('订单已取消')
+    when 'good_returned'
+      t('订单已退货')
+    end
+  end
+
 end
